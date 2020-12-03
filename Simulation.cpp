956316d21ddf257bbc9aac1827ad1db213b4d74a
richmondclaire <72709418+richmondclaire@users.noreply.github.com>
2020-12-02 21:54:17 -0500

    Update Simulation.cpp
    changed simulation to work for creating vehicles and going straight for right amount of time

--- Simulation.cpp
+++ Simulation.cpp
@@ -22,111 +22,147 @@ int main()
     std::vector<VehicleBase*> northbound(halfSize * 2 + 2, nullptr);
 
     char dummy;
-
-    // test drawing vehicles moving eastbound and westbound
-    VehicleBase vb1(VehicleType::car, Direction::east);
-    VehicleBase vb2(VehicleType::suv, Direction::east);
-    VehicleBase vb3(VehicleType::truck, Direction::east);
-    VehicleBase vb4(VehicleType::car, Direction::west);
-    VehicleBase vb5(VehicleType::suv, Direction::west);
-    VehicleBase vb6(VehicleType::truck, Direction::west);
-
-    VehicleBase vb7(VehicleType::car, Direction::south);
-    VehicleBase vb8(VehicleType::suv, Direction::south);
-    VehicleBase vb9(VehicleType::truck, Direction::south);
-    VehicleBase vb10(VehicleType::car, Direction::north);
-    VehicleBase vb11(VehicleType::suv, Direction::north);
-    VehicleBase vb12(VehicleType::truck, Direction::north);
-    VehicleBase vb13(vb12);  // just test the copy constructor -- should be #011
+    double random = // put random number generator here
     
-    int i = 0; // This will keep up with time
-
-    anim.setLightNorthSouth(LightColor::red);
-    anim.setLightEastWest(LightColor::green);
-
-
-    // if (eastbound[a]>easbound[6])...
-    for (; i < maximum_simulated_time; i++)
-    {   
-        // Move all vehiles by one 
-        northbound[i+1] = northbound[i]; // example spot 1 will now hold stuff from spot 0
-        southbound[i+1] = southbound[i];
-        westbound[i+1] = westbound[i];
-        eastbound[i+1] = eastbound[i];
-        
-        
-        // If random number <= prob_new_vehile_northbound AND space is open, create new vehile
-       
-        
-        eastbound[10+i] = eastbound[11+i] = &vb1;
-        eastbound[6+i] = eastbound[7+i] = eastbound[8+i] = &vb2;
-        eastbound[0+i] = eastbound[1+i] = eastbound[2+i] = eastbound[3+i] = &vb3;
-
-        westbound[9+i] = westbound[10+i] = &vb4;
-        westbound[6+i] = westbound[7+i] = westbound[8+i] = &vb5;
-        westbound[0+i] = westbound[1+i] = westbound[2+i] = westbound[3+i] = &vb6;
-
-        southbound[12+i+j] = southbound[13+i+j] = &vb7;
-        southbound[5+j] = southbound[6+j] = southbound[7+j] = &vb8;
-        southbound[0+j] = southbound[1+j] = southbound[2+j] = southbound[3+j] = &vb9;
-
-        northbound[12+i+j] = northbound[13+i+j] = &vb10;
-        northbound[5+j] = northbound[6+j] = northbound[7+j] = &vb11;
-        //northbound[0+j] = northbound[1+j] = northbound[2+j] = northbound[3+j] = &vb12;
-        northbound[0+j] = northbound[1+j] = northbound[2+j] = northbound[3+j] = &vb13;
-
-        anim.setVehiclesNorthbound(northbound);
-        anim.setVehiclesWestbound(westbound);
-        anim.setVehiclesSouthbound(southbound);
-        anim.setVehiclesEastbound(eastbound);
-
-        anim.draw(i);
-        std::cin.get(dummy);
-
-        southbound.assign(halfSize * 2 + 2, nullptr); // reset vector
-        northbound.assign(halfSize * 2 + 2, nullptr); // reset
-        eastbound.assign(halfSize * 2 + 2, nullptr); // reset
-        westbound.assign(halfSize * 2 + 2, nullptr); // reset
-
-        if (i == 1) anim.setLightEastWest(LightColor::yellow);
+    // CREATE NEW VEHICLE - NORTHBOUND
+    if ( random <= prob_new_vehicle_northbound ){ // If true, create new vehicle
+        if ( random <= proportion_of_cars ){
+        VehicleBase northCar(VehicleType::car, Direction::north); // create car
+            totalNumVehicles++;
+        }
+        else if ( random <= proportion_of_SUVs ){
+        VehicleBase northSUV(VehicleType::SUV, Direction north); // create suv
+            totalNumVehicles++;
+        }
+        else {
+        VehicleBase northTruck(VehicleType::Truck, Direction north); // create truck
+            totalNumVehicles++;
+        }
     }
+    
+    // CREATE NEW VEHICLE - SOUTHBOUND
+    if ( random <= prob_new_vehicle_southbound ){ // If true, create new vehicle
+        if ( random <= proportion_of_cars ){
+        VehicleBase southCar(VehicleType::car, Direction::south); // create car
+            totalNumVehicles++;
+        }
+        else if ( random <= proportion_of_SUVs ){
+        VehicleBase southSUV(VehicleType::SUV, Direction south); // create suv
+            totalNumVehicles++;
+        }
+        else {
+        VehicleBase southTruck(VehicleType::Truck, Direction south); // create truck
+            totalNumVehicles++;
+        }
+    }
+    
+    // CREATE NEW VEHICLE - WESTBOUND
+    if ( random <= prob_new_vehicle_westbound ){ // If true, create new vehicle
+        if ( random <= proportion_of_cars ){
+        VehicleBase westCar(VehicleType::car, Direction::west); // create car
+        totalNumVehicles++;
+        }
+        else if ( random <= proportion_of_SUVs ){
+        VehicleBase westSUV(VehicleType::SUV, Direction west); // create suv
+        totalNumVehicles++;
+        }
+        else {
+        VehicleBase westTruck(VehicleType::Truck, Direction west); // create truck
+        totalNumVehicles++;
+        }
+    }
+    
+    // CREATE NEW VEHICLE - EASTBOUND
+    if ( random <= prob_new_vehicle_eastbound ){ // If true, create new vehicle
+        if ( random <= proportion_of_cars ){
+        VehicleBase northCar(VehicleType::car, Direction::east); // create car
+        totalNumVehicles++;
+        }
+        else if ( random <= proportion_of_SUVs ){
+        VehicleBase northSUV(VehicleType::SUV, Direction east); // create suv
+        totalNumVehicles++;
+        }
+        else {
+        VehicleBase northTruck(VehicleType::Truck, Direction east); // create truck
+        totalNumVehicles++;
+        }
+    }
+    
+    
+    int i = 0; 
 
     anim.setLightNorthSouth(LightColor::green);
     anim.setLightEastWest(LightColor::red);
+    
+    while ( time < maximum_simulated_time) {
+    
+        for (; i < green_north_south + yellow_north_south; i++) // Run for length of green + yellow
+        {   
+            // If space ahead is avaliable, move N/S vehiles by one 
+            for ( int j = 0, j < numVehicles; j++){
+                if (northbound[j].canAdvance()){
+                northbound[i+1] = northbound[i]; // example spot 1 will now hold stuff from spot 0
+                }
+                if (southbound[j].canAdvance()){
+                southbound[i+1] = southbound[i];
+                }
+            }
 
-    for (; j < 5; j++)
-    {
-        eastbound[10+i+j] = eastbound[11+i+j] = &vb1;
-        eastbound[6+i+j] = eastbound[7+i+j] = eastbound[8+i+j] = &vb2;
-        eastbound[0+i] = eastbound[1+i] = eastbound[2+i] = eastbound[3+i] = &vb3;
+            // Make animation show new moves:
+            anim.setVehiclesNorthbound(northbound);
+            anim.setVehiclesWestbound(westbound);
+            anim.setVehiclesSouthbound(southbound);
+            anim.setVehiclesEastbound(eastbound);
 
-        westbound[9+i+j] = westbound[10+i+j] = &vb4;
-        westbound[6+i+j] = westbound[7+i+j] = westbound[8+i+j] = &vb5;
-        westbound[0+i] = westbound[1+i] = westbound[2+i] = westbound[3+i] = &vb6;
+            anim.draw(i);
+            std::cin.get(dummy);
 
-        southbound[12+i+j] = southbound[13+i+j] = &vb7;
-        southbound[5+j] = southbound[6+j] = southbound[7+j] = &vb8;
-        southbound[0+j] = southbound[1+j] = southbound[2+j] = southbound[3+j] = &vb9;
+            southbound.assign(halfSize * 2 + 2, nullptr); // reset vector
+            northbound.assign(halfSize * 2 + 2, nullptr); // reset
+            eastbound.assign(halfSize * 2 + 2, nullptr); // reset
+            westbound.assign(halfSize * 2 + 2, nullptr); // reset
 
-        southbound[12+i+j] = southbound[13+i+j] = &vb10;
-        northbound[5+j] = northbound[6+j] = northbound[7+j] = &vb11;
-        //northbound[0+j] = northbound[1+j] = northbound[2+j] = northbound[3+j] = &vb12;
-        northbound[0+j] = northbound[1+j] = northbound[2+j] = northbound[3+j] = &vb13;
+        } // When execution gets here, loop has ran for length of green and yellow
 
-        anim.setVehiclesNorthbound(northbound);
-        anim.setVehiclesWestbound(westbound);
-        anim.setVehiclesSouthbound(southbound);
-        anim.setVehiclesEastbound(eastbound);
+        // Now for the east and west to move
+        anim.setLightNorthSouth(LightColor::red);
+        anim.setLightEastWest(LightColor::green);
 
-        anim.draw(i+j);
-        std::cin.get(dummy);
 
-        southbound.assign(halfSize * 2 + 2, nullptr); // reset
-        northbound.assign(halfSize * 2 + 2, nullptr); // reset
-        eastbound.assign(halfSize * 2 + 2, nullptr); // reset
-        westbound.assign(halfSize * 2 + 2, nullptr); // reset
+            // If end of car is above index length of road, remove vehicle
 
-        if (j == 2) anim.setLightNorthSouth(LightColor::yellow);
-    }
+    int k = 0;
 
+            for (; k < green_east_west + yellow_east_west; k++) // Run for length of green + yellow
+        {   
+            // If space ahead is avaliable, move W/E vehiles by one 
+            for ( int j = 0, j < numVehicles; j++){
+                if (westbound[j].canAdvance()){
+                westbound[i+1] = westbound[i]; // example spot 1 will now hold stuff from spot 0
+                }
+                if (eastbound[j].canAdvance()){
+                eastbound[i+1] = eastbound[i];
+                }
+            }
+
+            // Make animation show new moves:
+            anim.setVehiclesNorthbound(northbound);
+            anim.setVehiclesWestbound(westbound);
+            anim.setVehiclesSouthbound(southbound);
+            anim.setVehiclesEastbound(eastbound);
+
+            anim.draw(i);
+            std::cin.get(dummy);
+
+            southbound.assign(halfSize * 2 + 2, nullptr); // reset vector
+            northbound.assign(halfSize * 2 + 2, nullptr); // reset
+            eastbound.assign(halfSize * 2 + 2, nullptr); // reset
+            westbound.assign(halfSize * 2 + 2, nullptr); // reset
+
+        } // When execution gets here, loop has ran for length of green and yellow
+
+            time = i + k;  
+        
+    } // when execution gets here, maximum simulation time is up.
+    
 }
